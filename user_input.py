6966e48dc96a9fc6ecf665f5eabd195459eea31b
Luis Sozinho <you@example.com>
2024-03-17 13:50:36 -0800

    list/arrasy

--- user_input.py
+++ user_input.py
@@ -1,5 +1,42 @@
-name = input("Whats you name: ")
-age = input("Your age: ")
-location = input("Your location: ")
+# name = input("Whats you name: ")
+# age = input("Your age: ")
+# location = input("Your location: ")
 
-print("Hello ",name, " you are ", age, " years old and you live in ", location )
+# print("Hello ",name, " you are ", age, " years old and you live in ", location )
+
+# if 'bar' in {'foo': 1, 'bar': 2, 'baz': 3}:
+#     print(1)
+#     print(2)
+#     if 'a' in 'qux':
+#         print(3)
+# print(4)
+
+# x = 10
+# y = 20
+
+# if x < y: print('foo'); print('bar'); print('baz')
+
+# if x < y: print('foo') else: print('bar')
+
+# if x < y: print('foo')
+# elif y < x: print('bar')
+# else: print('baz')
+
+
+# if x < y: 
+#     if x > 10: print('foo')
+
+# 'a' + 'x' if '123'.isdigit() else 'y' + 'b'
+
+
+# if (1, 2):
+# print('foo')
+
+if (1, 2): print('foo')
+
+if (1, 2):
+
+    print('foo')
+    
+if (1, 2):
+    print('foo')
